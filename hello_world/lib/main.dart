e6247d33e85ef839985b3a10c866346def07e276
zuhdalfan <muhammadzuhdialfathan@gmail.com>
2022-03-01 22:07:46 +0700

    add rating

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Hello World Demo Application',
       theme: ThemeData(
         primarySwatch: Colors.blue,),
-      home: MyHomePage2(title: 'Product List'),
+      home: MyHomePage(title: 'Product List'),
     );
   }
 }
@@ -181,12 +181,78 @@ class ProductBox extends StatelessWidget{
                       style: TextStyle(fontWeight: FontWeight.bold),),
                     Text(this.description, textAlign: TextAlign.center,),
                     Text("Price: "+this.price.toString()+" USD"),
+                    RatingBox(),
                   ],
                 ),
               ))
           ],
         ),
       ),
+    );
+  }
+}
+
+class RatingBox extends StatefulWidget{
+  @override 
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox>{
+  int _rating = 0;
+
+  void _setRatingAsOne(){
+    setState(() {
+      _rating = 1;
+    });
+  }
+  void _setRatingAsTwo(){
+    setState(() {
+      _rating = 2;
+    });
+  }
+  void _setRatingAsThree(){
+    setState(() {
+      _rating = 3;
+    });
+  }
+  
+  Widget build(BuildContext context){
+    double _size = 20;
+    print(_rating);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >=1?Icon(Icons.star,size: _size,):Icon(Icons.star_border,size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >=2?Icon(Icons.star,size: _size,):Icon(Icons.star_border,size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >=3?Icon(Icons.star,size: _size,):Icon(Icons.star_border,size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
     );
   }
 }
